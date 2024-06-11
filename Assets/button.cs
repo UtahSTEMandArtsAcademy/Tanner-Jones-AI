738113614cf9c410418a4c682a53721a918d9e60
UtahSTEMandArtsAcademy <153949503+UtahSTEMandArtsAcademy@users.noreply.github.com>
2024-06-11 15:51:23 -0600

    Finished (pause)

--- Assets/button.cs
+++ Assets/button.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class button : MonoBehaviour
 {
+    public bool isPaused = false;
+    public GameObject pauseMenu;
     public void sceneChanger(string name) {
         SceneManager.LoadScene(name);
     }
@@ -15,5 +17,19 @@ public class button : MonoBehaviour
 
     public void ResetTheScene() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            isPaused = !isPaused;
+            Time.timeScale = isPaused ? 0 : 1;
+        }
+        if(isPaused == true){
+            pauseMenu.SetActive(true);
+        }
+        else{
+            pauseMenu.SetActive(false);
+        }
     }
 }
